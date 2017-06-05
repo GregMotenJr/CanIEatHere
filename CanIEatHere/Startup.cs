d51ca1ca018ad4e82cc565c97bbfa1535518edbc
GregMotenJr <gmotenjr@gmail.com>
2017-06-05 12:17:14 -0400

    Initial Commit

---
+++ CanIEatHere/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CanIEatHere.Startup))]
+namespace CanIEatHere
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
