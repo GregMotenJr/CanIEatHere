00aa7435e58e0bea0e8fb23bc9705d9f5edee5d6
ansulez <ansulez@gmail.com>
2017-06-12 20:43:45 -0400

    playing with jQuery

---
+++ CanIEatHere/Scripts/custom-jquery.js
@@ -0,0 +1,12 @@
+﻿// JavaScript source code
+$(document).ready(function () {
+    //BACKGROUND:
+    //TRANSLUCENT BOX:
+    $("#testDiv").slideDown("slow");
+    //LINES:
+    //$("#box1").delay(3000).animate({ width: "50%" }, 3000);
+    //$("#box2").delay(3000).animate({ width: "50%" }, 3000);
+    //TEXT:
+    $("#text1").fadeIn(1500);
+    $("#text2").delay(1000).fadeIn(1500);
+});
