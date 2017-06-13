bc97ec802fa757bd4a1628d5324a03d3be89dea2
Liberty Wesley <artisticliberties0821@gmail.com>
2017-06-13 10:05:29 -0400

    updated create new review page by including improved search functionality for restaurant field

--- CanIEatHere/Models/PlacesAPI.cs
+++ CanIEatHere/Models/PlacesAPI.cs
@@ -11,8 +11,6 @@ namespace CanIEatHere.Models
     {
         public Object getPlaceID(string searchString)
         {
-            //string searchString = "barrio";
-            //string url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyBk0uA5zKZMDJmD5WUVLJq2gosxwV5GM_c";
             string url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="+searchString+ "&location=41.4993,-81.69442&radius=32187&type=restaurant&key=AIzaSyBk0uA5zKZMDJmD5WUVLJq2gosxwV5GM_c";
             var client = new WebClient();
             var content = client.DownloadString(url);
