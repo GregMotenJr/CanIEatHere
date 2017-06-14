342d88d6b0e4f7e47698e13b9dca0b1e60a4b19b
ansulez <ansulez@gmail.com>
2017-06-14 14:44:55 -0400

    Fixed home jQuery.

--- CanIEatHere/Scripts/custom-jquery.js
+++ CanIEatHere/Scripts/custom-jquery.js
@@ -1,4 +1,10 @@
 ﻿//jQuery for 'About' page:
 $(document).ready(function () {
+    //ANIMATED LINE:
+    $("#box2").delay(2200).fadeIn(100).animate({ width: "50%" }, 3000);
+    //TEXT FADE-IN:
+    $("#text1").fadeIn(1000);
+    $("#text2").delay(500).fadeIn(1200);
+    //BACKGROUND FADE-IN:
     $('#back').animate({ opacity: 0 }, 0).css({ 'background-image': 'url (https://cdn.pixabay.com/photo/2017/04/11/17/54/appetizers-2222361_960_720.jpg)' }).animate({ opacity: 1 }, 2500);
 });
