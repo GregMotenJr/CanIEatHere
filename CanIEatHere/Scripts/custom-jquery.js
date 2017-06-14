939fbefdbeecc5f62efa19687a1df86fc79883e2
ansulez <ansulez@gmail.com>
2017-06-14 10:38:54 -0400

    About page background fade-in

--- CanIEatHere/Scripts/custom-jquery.js
+++ CanIEatHere/Scripts/custom-jquery.js
@@ -1,9 +1,8 @@
-﻿//jQuery for home page:
+﻿//jQuery for 'Home' page:
 $(document).ready(function () {
     //ANIMATED LINE:
-    $("#box2").delay(2200).fadeIn(100).animate({ width: "50%" }, "slow");
-    //FADE-IN:
+    $("#box2").delay(2200).fadeIn(100).animate({ width: "50%" }, 3000);
+    //TEXT FADE-IN:
     $("#text1").fadeIn(1000);
     $("#text2").delay(500).fadeIn(1200);
-    $("#arrow-button").fadeIn(2000);
 });
