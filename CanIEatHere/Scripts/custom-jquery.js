9664ecc0dc2a0f7a4389cdb18997beb808acca9e
ansulez <ansulez@gmail.com>
2017-06-14 14:38:35 -0400

    messing with backgrounds

--- CanIEatHere/Scripts/custom-jquery.js
+++ CanIEatHere/Scripts/custom-jquery.js
@@ -5,4 +5,6 @@ $(document).ready(function () {
     //TEXT FADE-IN:
     $("#text1").fadeIn(1000);
     $("#text2").delay(500).fadeIn(1200);
+    //BACKGROUND FADE-IN:
+    $('#back').animate({ opacity: 0 }, 0).css({ 'background-image': 'url (https://cdn.pixabay.com/photo/2016/10/31/18/16/salad-1786313_960_720.jpg)' }).animate({ opacity: 1 }, 2500);
 });
