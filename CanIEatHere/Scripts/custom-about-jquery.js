447ceed3c0993cd9fb1a5011545f160acbdb0962
ansulez <ansulez@gmail.com>
2017-06-14 14:42:45 -0400

    The image works now. Fetch styling branch.

--- CanIEatHere/Scripts/custom-about-jquery.js
+++ CanIEatHere/Scripts/custom-about-jquery.js
@@ -1,5 +1,4 @@
-﻿/// <reference path="custom-about-jquery.js" />
-//jQuery for 'About' page:
+﻿//jQuery for 'About' page:
 $(document).ready(function () {
     $('#back').animate({ opacity: 0 }, 0).css({ 'background-image': 'url (https://cdn.pixabay.com/photo/2017/04/11/17/54/appetizers-2222361_960_720.jpg)' }).animate({opacity: 1 }, 2500);
 });
--- CanIEatHere/Scripts/custom-jquery.js
+++ CanIEatHere/Scripts/custom-jquery.js
@@ -1,10 +1,4 @@
-﻿//jQuery for 'Home' page:
+﻿//jQuery for 'About' page:
 $(document).ready(function () {
-    //ANIMATED LINE:
-    $("#box2").delay(2200).fadeIn(100).animate({ width: "50%" }, 3000);
-    //TEXT FADE-IN:
-    $("#text1").fadeIn(1000);
-    $("#text2").delay(500).fadeIn(1200);
-    //BACKGROUND FADE-IN:
-    $('#back').animate({ opacity: 0 }, 0).css({ 'background-image': 'url (https://cdn.pixabay.com/photo/2016/10/31/18/16/salad-1786313_960_720.jpg)' }).animate({ opacity: 1 }, 2500);
+    $('#back').animate({ opacity: 0 }, 0).css({ 'background-image': 'url (https://cdn.pixabay.com/photo/2017/04/11/17/54/appetizers-2222361_960_720.jpg)' }).animate({ opacity: 1 }, 2500);
 });
