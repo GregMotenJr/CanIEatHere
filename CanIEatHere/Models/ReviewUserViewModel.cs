869e9e06e2521632ac2389d9cb71ea9a7a5edd44
Liberty Wesley <artisticliberties0821@gmail.com>
2017-06-14 11:40:19 -0400

    continues editing details page

---
+++ CanIEatHere/Models/ReviewUserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CanIEatHere.Models
+{
+    public class ReviewUserViewModel
+    {
+        public Restaurant restaurant { get; set; }
+        public AspNetUser user { get; set; }
+    }
+}
