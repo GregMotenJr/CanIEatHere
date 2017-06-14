6a719ff38fee4efd796d57298ed7ea9f51103a18
ansulez <ansulez@gmail.com>
2017-06-14 12:48:46 -0400

    styling

---
+++ CanIEatHere/Scripts/custom-about-jquery.js
@@ -0,0 +1,4 @@
+﻿//jQuery for 'About' page:
+$(document).ready(function () {
+    $('#back').animate({ opacity: 0 }, 0).css({ 'background-image': 'url (https://cdn.pixabay.com/photo/2017/04/11/17/54/appetizers-2222361_960_720.jpg)' }).animate({opacity: 1 }, 2500);
+});
