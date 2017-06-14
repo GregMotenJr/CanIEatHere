b097fdf75e883d46c4b6b857a566f85faff0a4e1
ansulez <ansulez@gmail.com>
2017-06-14 19:06:14 -0400

    Trying to resolve jQuery issue on About page.

---
+++ CanIEatHere/Scripts/custom-jquery/homepage_jquery.js
@@ -0,0 +1,8 @@
+﻿//jQuery for 'Home' page:
+$(document).ready(function () {
+    //TEXT FADE-IN:
+    $("#text1").delay(1000).fadeIn(1000);
+    $("#text2").delay(1300).fadeIn(1500);
+    //ANIMATED LINE:
+    $("#box2").delay(1700).fadeIn(100).animate({ width: "50%" }, 1500);
+});
