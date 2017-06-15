d7c97f1a0409c849837a75aa9642b0fa9bea82de
ansulez <ansulez@gmail.com>
2017-06-15 10:50:01 -0400

    Animation for About page.

--- CanIEatHere/Scripts/custom-jquery-about.js
+++ CanIEatHere/Scripts/custom-jquery-about.js
@@ -1,4 +1,7 @@
 ﻿//jQuery for 'About' page:
 $(document).ready(function () {
+    //BACKGROUND:
     $('#back').animate({ opacity: 0 }, 0).css({ 'background-image': 'url(https://preview.ibb.co/nvkmiQ/appetizers_2222361_1280.jpg)' }).animate({ opacity: 1 }, 2500);
+    //GRID:
+    $('#aboutUsGrid').delay(1000).fadeIn(3000);
 });
