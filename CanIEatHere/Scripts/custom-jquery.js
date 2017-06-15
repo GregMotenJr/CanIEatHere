16465424675570e03e3eadbc2f642a93b95056e2
ansulez <ansulez@gmail.com>
2017-06-14 20:58:41 -0400

    Animated the homepage's background.

--- CanIEatHere/Scripts/custom-jquery.js
+++ CanIEatHere/Scripts/custom-jquery.js
@@ -5,4 +5,6 @@ $(document).ready(function () {
     $("#text2").delay(1300).fadeIn(1500);
     //ANIMATED LINE:
     $("#box2").delay(1700).fadeIn(100).animate({ width: "50%" }, 1500);
+    //ANIMATED BACKGROUND:
+    $('#back').animate({ opacity: 0 }, 0).css({ 'background-image': 'url(https://preview.ibb.co/hONP3Q/home_about_artichokes.jpg)' }).animate({ opacity: 1 }, 2500);
 });
