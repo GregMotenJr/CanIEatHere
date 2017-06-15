0c4b6a90ef67a2de2e790219ae648150558c80b8
ansulez <ansulez@gmail.com>
2017-06-14 20:04:08 -0400

    Unable to resolve jQuery issue due to error.

--- CanIEatHere/Scripts/custom-jquery/aboutpage_jquery.js
+++ CanIEatHere/Scripts/custom-jquery/aboutpage_jquery.js
@@ -1,5 +1,6 @@
 ﻿//jQuery for 'About' page:
 $(document).ready(function () {
+    $("#text3").delay(1000).fadeIn(1000);
     $('#back').animate({ opacity: 0 }, 0).css({ 'background-image': 'url (https://cdn.pixabay.com/photo/2017/04/11/17/54/appetizers-2222361_960_720.jpg)' }).animate({ opacity: 1 }, 2500);
 });
 
