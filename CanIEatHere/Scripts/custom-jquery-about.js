fb139c2f761564715adef9b1113849a453efa050
ansulez <ansulez@gmail.com>
2017-06-14 20:36:23 -0400

    Resolved jQuery .animate() opacity issue on 'About' page.

--- CanIEatHere/Scripts/custom-jquery-about.js
+++ CanIEatHere/Scripts/custom-jquery-about.js
@@ -1,4 +1,4 @@
 ﻿//jQuery for 'About' page:
 $(document).ready(function () {
-    $('#back').animate({ opacity: 0 }, 0).css({ 'background-image': 'url(https://cdn.pixabay.com/photo/2017/04/11/17/54/appetizers-2222361_960_720.jpg)' }).animate({ opacity: 1 }, 2500);
+    $('#back').animate({ opacity: 0 }, 0).css({ 'background-image': 'url(https://preview.ibb.co/nvkmiQ/appetizers_2222361_1280.jpg)' }).animate({ opacity: 1 }, 2500);
 });
