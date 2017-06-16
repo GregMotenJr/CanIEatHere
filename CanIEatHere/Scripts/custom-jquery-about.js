cf1433d10f8ab6dbd386937101cae6fbd1f71735
ansulez <ansulez@gmail.com>
2017-06-15 20:55:58 -0400

    Playing with animate.css.

--- CanIEatHere/Scripts/custom-jquery-about.js
+++ CanIEatHere/Scripts/custom-jquery-about.js
@@ -2,34 +2,5 @@
 $(document).ready(function () {
     //BACKGROUND:
     $("#back").animate({ opacity: 0 }, 0).css({ "background-image": "url(https://preview.ibb.co/nvkmiQ/appetizers_2222361_1280.jpg)" }).animate({ opacity: 1 }, 2500);
-    //GRID:
-    $("#aboutUsGrid").delay(1000).fadeIn(3000);
 });
-
-$(document).ready(function () {
-    //ABOUT US DIVS
-    var effect = "slide";
-    var options = { direction: "right" };
-    var duration = 1000;
-    $("#Megan").delay(1000).toggle(effect, options, duration);
-    $("#Greg").delay(1100).toggle(effect, options, duration);
-    $("#Anthony").delay(1200).toggle(effect, options, duration);
-    $("#Liberty").delay(1300).toggle(effect, options, duration);
-});
-
-
-
-
-//$(".myButton").click(function () {
-
-//    var effect = "slide";
-//    var options = { direction: "left" }
-//    var duration = 1000;
-
-//    $("#Megan").toggle(effect, options, duration);
-//    $("#Greg").delay(500).toggle(effect, options, duration);
-//    $("#Anthony").delay(1000).toggle(effect, options, duration);
-//    $("#Liberty").delay(1500).toggle(effect, options, duration);
-
-//});
 
