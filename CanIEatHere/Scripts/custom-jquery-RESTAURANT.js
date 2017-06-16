fcf9c3cdeba956312df112fa1cc7584bb700db2c
ansulez <ansulez@gmail.com>
2017-06-16 11:50:37 -0400

    about

--- CanIEatHere/Scripts/custom-jquery-RESTAURANT.js
+++ CanIEatHere/Scripts/custom-jquery-RESTAURANT.js
@@ -1,4 +1,4 @@
 ﻿//jQuery for 'Restaurants' page:
 $(document).ready(function () {
-    $('#back').animate({ opacity: 0 }, 0).css({ 'background-image': 'url(https://preview.ibb.co/nmq8xk/top_view_1248955_1280_1.jpg)' }).animate({ opacity: 1 }, 2500);
+    $('#back').animate({ opacity: 0 }, 0).css({ 'background-image': 'url(https://preview.ibb.co/nvkmiQ/appetizers_2222361_1280.jpg)' }).animate({ opacity: 1 }, 2500);
 });
--- CanIEatHere/Scripts/custom-jquery-about.js
+++ CanIEatHere/Scripts/custom-jquery-about.js
@@ -1,6 +1,10 @@
 ﻿//jQuery for 'About' page:
-//$(document).ready(function () {
-//    //BACKGROUND:
-//    $("#back").animate({ opacity: 0 }, 0).css({ "background-image": "url(https://preview.ibb.co/nvkmiQ/appetizers_2222361_1280.jpg)" }).animate({ opacity: 1 }, 2500);
-//});
+$(document).ready(function () {
+    //BACKGROUND:
+    $("#back").animate({ opacity: 0 }, 0).css({ "background-image": "url(https://preview.ibb.co/hWtif5/salad_1710328_960_720.jpg)" }).animate({ opacity: 1 }, 2500);
+});
 
+//https://preview.ibb.co/eRrGYQ/Tower_City.jpg
+//https://preview.ibb.co/nmq8xk/top_view_1248955_1280_1.jpg
+//https://preview.ibb.co/hWtif5/salad_1710328_960_720.jpg
+//https://preview.ibb.co/nhGatQ/sky_232000_960_720.jpg
