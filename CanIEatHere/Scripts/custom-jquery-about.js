01dc27e0fc959386bcc8ace9ce5da0da6fcbac7d
ansulez <ansulez@gmail.com>
2017-06-16 11:17:08 -0400

    About page

--- CanIEatHere/Scripts/custom-jquery-about.js
+++ CanIEatHere/Scripts/custom-jquery-about.js
@@ -1,6 +1,6 @@
 ﻿//jQuery for 'About' page:
-$(document).ready(function () {
-    //BACKGROUND:
-    $("#back").animate({ opacity: 0 }, 0).css({ "background-image": "url(https://preview.ibb.co/nvkmiQ/appetizers_2222361_1280.jpg)" }).animate({ opacity: 1 }, 2500);
-});
+//$(document).ready(function () {
+//    //BACKGROUND:
+//    $("#back").animate({ opacity: 0 }, 0).css({ "background-image": "url(https://preview.ibb.co/nvkmiQ/appetizers_2222361_1280.jpg)" }).animate({ opacity: 1 }, 2500);
+//});
 
