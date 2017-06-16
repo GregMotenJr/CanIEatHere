272e5445f2da7d60d5a3a772a253b99a70dcd905
ansulez <ansulez@gmail.com>
2017-06-16 13:42:36 -0400

    zoomIn About

--- CanIEatHere/Scripts/custom-jquery-about.js
+++ CanIEatHere/Scripts/custom-jquery-about.js
@@ -1,9 +1,12 @@
 ﻿//jQuery for 'About' page:
 $(document).ready(function () {
-    //BACKGROUND:
-    $("#back").animate({ opacity: 0 }, 0).css({ "background-image": "url(https://preview.ibb.co/hWtif5/salad_1710328_960_720.jpg)" }).animate({ opacity: 1 }, 2500);
+    //background:
+    //$("#back").animate({ opacity: 0 }, 0).css({ "background-image": "url()" }).animate({ opacity: 1 }, 2500);
+    //divs:
+    $("#LibertyAbout").fadeIn(1500);
 });
 
+//https://preview.ibb.co/hWtif5/salad_1710328_960_720.jpg
 //https://preview.ibb.co/eRrGYQ/Tower_City.jpg
 //https://preview.ibb.co/nmq8xk/top_view_1248955_1280_1.jpg
 //https://preview.ibb.co/hWtif5/salad_1710328_960_720.jpg
