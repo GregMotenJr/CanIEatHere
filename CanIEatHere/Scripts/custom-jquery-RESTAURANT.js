4816ca7151ccb37fcd895c7af41d7f1bd8193b32
ansulez <ansulez@gmail.com>
2017-06-18 18:27:42 -0400

    random styling

--- CanIEatHere/Scripts/custom-jquery-RESTAURANT.js
+++ CanIEatHere/Scripts/custom-jquery-RESTAURANT.js
@@ -1,4 +1,4 @@
 ﻿//jQuery for 'Restaurants' page:
-$(document).ready(function () {
-    $('#back').animate({ opacity: 0 }, 0).css({ 'background-image': 'url(https://preview.ibb.co/nvkmiQ/appetizers_2222361_1280.jpg)' }).animate({ opacity: 1 }, 2500);
-});
+//$(document).ready(function () {
+//    $('#back').animate({ opacity: 0 }, 0).css({ 'background-image': 'url(https://preview.ibb.co/nvkmiQ/appetizers_2222361_1280.jpg)' }).animate({ opacity: 1 }, 2500);
+//});
--- CanIEatHere/Scripts/custom-jquery-REVIEW.js
+++ CanIEatHere/Scripts/custom-jquery-REVIEW.js
@@ -1,4 +1,4 @@
 ﻿//jQuery for 'Reviews' page:
-$(document).ready(function () {
-    $('#back').animate({ opacity: 0 }, 0).css({ 'background-image': 'url(https://preview.ibb.co/hoovck/basil_594168_1280.jpg)' }).animate({ opacity: 1 }, 2500);
-});
+//$(document).ready(function () {
+//    $('#back').animate({ opacity: 0 }, 0).css({ 'background-image': 'url(https://preview.ibb.co/hoovck/basil_594168_1280.jpg)' }).animate({ opacity: 1 }, 2500);
+//});
